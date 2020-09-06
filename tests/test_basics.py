9020a42df4c7cc744700a696668ca376463ff161
Yushi OMOTE <yushiomote@gmail.com>
2020-09-06 16:18:22 +0900

    Add dict/list tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -35,16 +35,33 @@ PRIMITIVES = [
     Entry(bytearray, [bytearray(b'abc\x03'), bytearray(b'')])
 ]
 
+LISTS = [
+    Entry(List[bool], [[True, False], []]),
+    Entry(List[int], [[-18, 0, 5], []]),
+    Entry(List[float], [[-3.1415, 0.0, 1.4142], []]),
+    Entry(List[str], [["wazzaaa", ""], []]),
+    Entry(List[bytes], [[b'abc\x03', b''], []]),
+    Entry(List[bytearray], [[bytearray(b'abc\x03'), bytearray(b'')], []])
+]
+
+DICTS_SK = [
+    Entry(Dict[str, bool], [{"k": True}, {}]),
+    Entry(Dict[str, int], [{"a": 3}, {}]),
+    Entry(Dict[str, float], [{"v": -1.4, "p": 0, "n": 2.2}, {}]),
+    Entry(Dict[str, str], [{"v": "avc", "p": ""}, {"n": "x"}, {}]),
+    Entry(Dict[str, bytes], [{"v": b"aaaa", "z": b""}, {"p": b"v"}, {}]),
+]
+
 @pytest.mark.parametrize("t1,v1", expand(PRIMITIVES))
 def test_primitives(t1, v1):
     repack(t1, v1)
 
-@pytest.mark.parametrize("t1,v1", expand(PRIMITIVES))
-@pytest.mark.parametrize("t2,v2", expand(PRIMITIVES))
+@pytest.mark.parametrize("t1,v1", expand(PRIMITIVES + LISTS + DICTS_SK))
+@pytest.mark.parametrize("t2,v2", expand(PRIMITIVES + LISTS + DICTS_SK))
 def test_simple_classes(t1, t2, v1, v2):
     @dataclass
-    class Simple:
+    class Test:
         a: t1
         b: t2
 
-    repack(Simple, v1, v2)
+    repack(Test, v1, v2)
