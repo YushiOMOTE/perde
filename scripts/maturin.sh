a5628f9d8b7737a7f7ee824ed4b55cc6a158bb49
YushiOMOTE <yushiomote@gmail.com>
2020-11-01 07:59:32 +0900

    Add build and publish logic in maturin.sh

--- scripts/maturin.sh
+++ scripts/maturin.sh
@@ -2,9 +2,20 @@
 
 set -eux
 
+cmd=$1
+
 for d in "perde" "perde-json" "perde-msgpack" "perde-yaml"; do
 	echo "Building/installing $d..."
 	pushd $d
-	maturin develop --release
+  case $1 in
+      "develop")
+	        maturin develop --release;;
+      "build")
+          maturin build --release;;
+      "publish")
+          maturin publish;;
+      "test-publish")
+          maturin publish -r https://test.pypi.org/simple/;;
+  esac
 	popd
 done
