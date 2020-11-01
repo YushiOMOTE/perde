16ec8c3053c0b3cabd7cfb99ca47c4d9e86a951a
YushiOMOTE <yushiomote@gmail.com>
2020-11-01 10:57:19 +0900

    Pass username and password to publish command

--- scripts/maturin.sh
+++ scripts/maturin.sh
@@ -13,9 +13,11 @@ for d in "perde" "perde-json" "perde-msgpack" "perde-yaml"; do
       "build")
           maturin build --release;;
       "publish")
-          maturin publish;;
+          maturin publish -u $PYPI_USER -p $PYPI_PASSWORD;;
       "test-publish")
-          maturin publish -r https://test.pypi.org/simple/;;
+          maturin publish -u $PYPI_USER -p $PYPI_PASSWORD -r https://test.pypi.org/legacy/;;
+      "link-readme")
+          ln -s ../README.md README.md
   esac
 	popd
 done
