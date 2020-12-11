b2b00b03aa628ea168c5ec9479a5761884d12675
YushiOMOTE <yushiomote@gmail.com>
2020-12-11 20:05:26 +0900

    Fix the issue of `import perde.submodule` not working

--- perde/perde/__init__.py
+++ perde/perde/__init__.py
@@ -1,6 +1,14 @@
 from .perde import resolve  # type: ignore
 from .perde import json, toml, yaml, msgpack  # noqa: F401
+import sys
 import enum
+
+
+# Workaround in https://github.com/PyO3/pyo3/issues/759
+sys.modules["perde.json"] = json
+sys.modules["perde.msgpack"] = msgpack
+sys.modules["perde.yaml"] = yaml
+sys.modules["perde.toml"] = toml
 
 
 def attr(*args, **kwargs):
